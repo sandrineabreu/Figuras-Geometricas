f735b2f9a77dd623755f5fa26980780f22812a26
sandrine <sandrine.abreu03@aluno.ifce.edu.br>
2025-05-19 22:17:17 -0300

    Add files via upload

---
+++ Triangulo.java
@@ -0,0 +1,39 @@
+package br.edu.figurasgeometricasplanas;
+
+public class Triangulo {
+	private double base;
+	private double altura;
+	private double area;
+
+		public Triangulo() {
+			base = 0;
+			altura = 0;
+			area = 0;
+		}
+		
+		public void recebaAltura(double altura) {
+			this.altura = altura; 
+		}
+		
+		public void recebaBase(double base) {
+			this.base = base; 
+		}
+		
+		
+		public void calcArea() {
+			area = (base * altura)/2;
+		}
+		public void mostrarArea () {
+			System.out.println("A área do Triângulo é: " + area);
+		}
+		
+		public double mostrarAreaComRetorno () {
+			System.out.println(area);
+			return area;
+		}
+
+
+
+	}
+	
+
