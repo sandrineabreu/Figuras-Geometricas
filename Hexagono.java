7d337be08c7e72e39c88d3363178fa40d7941d61
sandrine <sandrine.abreu03@aluno.ifce.edu.br>
2025-05-19 22:18:50 -0300

    Add files via upload

---
+++ Hexagono.java
@@ -0,0 +1,31 @@
+package br.edu.figurasgeometricasplanas;
+
+public class Hexagono {
+
+			private double lado;
+			private double area;
+			
+			public Hexagono() {
+				lado = 0;
+				area = 0;
+			}
+			
+			public void recebaLado(double lado) {
+				this.lado = lado; 
+			}
+			
+			public void calcArea() {
+				area = (3 * Math.sqrt(3) * lado * lado) / 2;
+			}
+			
+			public void mostrarArea () {
+				System.out.println("A área do Hexagono é: " + area);
+			}
+
+			public double mostrarAreaComRetorno () {
+				System.out.println(area);
+				return area;
+			}
+		
+		
+	}
